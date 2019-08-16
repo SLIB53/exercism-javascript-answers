9ced69bc17f23b6dd6e03844b4aff8463bf8e12f
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2019-08-16 00:21:32 -0400

    Add Collatz Conjecture

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,15 @@
+function steps_impl(n, counter = 0) {
+  if (n === 1) {
+    return counter;
+  }
+
+  return steps_impl(n % 2 === 0 ? n / 2 : 3 * n + 1, counter + 1);
+}
+
+export const steps = n => {
+  if (!isFinite(n) || n <= 0) {
+    throw "Only positive numbers are allowed";
+  }
+
+  return steps_impl(n);
+};
