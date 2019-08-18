4967e989492a4802d2fc994cc4d68c96c35f8ea5
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2019-08-17 22:13:30 -0400

    Cleanup simple-cipher

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,13 +1,12 @@
-import { isString } from "util";
-
-var crypto = require("crypto");
+import { isString } from 'util';
+import crypto from 'crypto';
 
 function generateRandomLetter() {
   /**
    * Generate a secure random byte,
    * wrap within the range 0-25 inclusive,
    * add to ASCII 'a' (value of 97).
-   * 
+   *
    * Note that since this maps 256 values to 26 values,
    * and 256 is not evenly divisible by 26,
    * some values will become slightly less common than others.
@@ -22,7 +21,7 @@ function generateRandomLetters(numLetters) {
 }
 
 function generateRandomKey(keyLength) {
-  return generateRandomLetters(keyLength).join("");
+  return generateRandomLetters(keyLength).join('');
 }
 
 function shiftDistanceFromKey(key, i) {
@@ -31,33 +30,32 @@ function shiftDistanceFromKey(key, i) {
 
 function encode(key, message) {
   return message
-    .split("")
+    .split('')
     .map((c, i) => {
       let alphabetIndex = c.charCodeAt(0) - 97 + shiftDistanceFromKey(key, i);
-      alphabetIndex = alphabetIndex % 26;
+      alphabetIndex %= 26;
       return String.fromCharCode(97 + alphabetIndex);
     })
-    .join("");
+    .join('');
 }
 
 function decode(key, message) {
   return message
-    .split("")
+    .split('')
     .map((c, i) => {
-      let reverseAlphabetIndex =
-        122 - c.charCodeAt(0) + shiftDistanceFromKey(key, i);
-      reverseAlphabetIndex = reverseAlphabetIndex % 26;
+      let reverseAlphabetIndex = 122 - c.charCodeAt(0) + shiftDistanceFromKey(key, i);
+      reverseAlphabetIndex %= 26;
       return String.fromCharCode(122 - reverseAlphabetIndex);
     })
-    .join("");
+    .join('');
 }
 
 function isValidKey(candidate) {
-  if (!isString(candidate) || candidate.trim() === "") {
+  if (!isString(candidate) || candidate.trim() === '') {
     return false;
   }
 
-  return candidate.split("").every(c => c >= "a" && c <= "z");
+  return candidate.split('').every(c => c >= 'a' && c <= 'z');
 }
 
 class Cipher {
@@ -65,7 +63,7 @@ class Cipher {
     const keyCandidate = key !== undefined ? key : generateRandomKey(100);
 
     if (!isValidKey(keyCandidate)) {
-      throw "Bad key";
+      throw new Error('Bad key');
     }
 
     this.key = keyCandidate;
