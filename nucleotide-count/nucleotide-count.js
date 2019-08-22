5e854933d225b912df47dc6ad5463711a2de6c73
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2019-08-22 02:36:32 -0400

    Update nucleotide count, reduce allocations

--- nucleotide-count/nucleotide-count.js
+++ nucleotide-count/nucleotide-count.js
@@ -8,11 +8,11 @@ export class NucleotideCounts {
   static parse(strand) {
     let counters = { A: 0, C: 0, G: 0, T: 0 };
 
-    strand.split("").forEach(nucleotide => {
-      if (!(counters[nucleotide] += 1)) {
+    for (let i = 0; i < strand.length; i++) {
+      if (!(counters[strand.charAt(i)] += 1)) {
         throw "Invalid nucleotide in strand";
       }
-    });
+    }
 
     return `${counters.A} ${counters.C} ${counters.G} ${counters.T}`;
   }
