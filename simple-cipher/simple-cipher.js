36d47d8258c73495374fbbe2a8c560a86cc69c8d
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2019-10-07 21:23:16 -0400

    Rewrite simple cipher package, reformat source

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
 /**
  * Generate a secure random byte,
@@ -9,36 +9,40 @@ import crypto from 'crypto';
  * and 256 is not evenly divisible by 26,
  * a few values will become slightly more common than others.
  */
-const generateRandomLetter = () => String.fromCharCode((crypto.randomBytes(1)[0] % 26) + 97);
+const generateRandomLetter = () =>
+  String.fromCharCode((crypto.randomBytes(1)[0] % 26) + 97);
 
-const generateRandomLetters = numLetters => Array.from(
-  { length: numLetters }, generateRandomLetter,
-);
+const generateRandomLetters = numLetters =>
+  Array.from({ length: numLetters }, generateRandomLetter);
 
-const generateRandomKey = keyLength => generateRandomLetters(keyLength).join('');
+const generateRandomKey = keyLength =>
+  generateRandomLetters(keyLength).join("");
 
 const getShiftDistance = (key, i) => key.charCodeAt(i % key.length) - 97;
 
-const encode = (key, message) => Array.from(message, (c, i) => {
-  let alphabetIndex = c.charCodeAt(0) - 97 + getShiftDistance(key, i);
-  alphabetIndex %= 26;
-  return String.fromCharCode(97 + alphabetIndex);
-}).join('');
+const encode = (key, message) =>
+  Array.from(message, (c, i) => {
+    let alphabetIndex = c.charCodeAt(0) - 97 + getShiftDistance(key, i);
+    alphabetIndex %= 26;
+    return String.fromCharCode(97 + alphabetIndex);
+  }).join("");
 
-const decode = (key, message) => Array.from(message, (c, i) => {
-  let reverseAlphabetIndex = 122 - c.charCodeAt(0) + getShiftDistance(key, i);
-  reverseAlphabetIndex %= 26;
-  return String.fromCharCode(122 - reverseAlphabetIndex);
-}).join('');
+const decode = (key, message) =>
+  Array.from(message, (c, i) => {
+    let reverseAlphabetIndex = 122 - c.charCodeAt(0) + getShiftDistance(key, i);
+    reverseAlphabetIndex %= 26;
+    return String.fromCharCode(122 - reverseAlphabetIndex);
+  }).join("");
 
-const isValidKey = candidate => typeof candidate === 'string'
-  && candidate.trim() !== ''
-  && /^[a-z]*$/.test(candidate);
+const isValidKey = candidate =>
+  typeof candidate === "string" &&
+  candidate.trim() !== "" &&
+  /^[a-z]*$/.test(candidate);
 
 export class Cipher {
   constructor(key = generateRandomKey(100)) {
     if (!isValidKey(key)) {
-      throw new Error('Bad key');
+      throw new Error("Bad key");
     }
 
     this.key = key;
